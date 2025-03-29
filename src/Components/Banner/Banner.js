be1230306b10efa61a9af325645a43f23240ee38
Biswa-bhusan <biswabhusanparida52@gmail.com>
2025-03-29 13:13:37 +0530

    Add files via upload

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import sliderImg from "../../assets/images/slider/1.png";
+import sliderImg1 from "../../assets/images/slider/2.png";
+import "../Banner/banner.css"
+
+const Banner = () => {
+  return (
+    <>
+      <section className="slider">
+        <Carousel variant="dark">
+          <Carousel.Item>
+            <img src={sliderImg} className="d-block w-100" alt="First slide" />
+            <Carousel.Caption>
+              <div className="slider_des">
+                <h5 className="heading">
+                  JOURNEY TO <span>EXPLORE WORLD</span>
+                </h5>
+                <p className="sub_text">
+                "The world is a book, and those who do not travel read only one page." - Saint Augustine
+                </p>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img src={sliderImg1} className="d-block w-100" alt="First slide" />
+            <Carousel.Caption>
+              <div className="slider_des">
+                <h5 className="heading">
+                  BEAUTIFUL PLACE <span>TO VISIT</span>
+                </h5>
+                <p className="sub_text">
+                "Travel makes one modest. You see what a tiny place you occupy in the world." - Gustave Flaubert
+                </p>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </section>
+    </>
+  );
+};
+
+export default Banner;
